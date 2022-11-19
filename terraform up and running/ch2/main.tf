41fb4bcc87c8fb52defb21a3f7a1f533aaab842f
jeff buenting <jeffbuenting@gmail.com>
2022-11-19 12:44:40 -0500

    removed provider file and added to main

--- terraform up and running/ch2/main.tf
+++ terraform up and running/ch2/main.tf
@@ -1,6 +1,23 @@
+
+terraform {
+    required_providers {
+        aws = {
+            source = "hasicorp/aws"
+            version = "-> 4.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+
 resource "aws_instance" "example" {
-    ami = "ami-08c40ec9ead489470"
+    # ami = "ami-08c40ec9ead489470"
+    ami = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
+
     vpc_security_group_ids = [aws_security_group.instance.id]
 
     user_data = <<-EOF
