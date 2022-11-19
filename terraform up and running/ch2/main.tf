8ed1b4d4a89b393b67729c8459c08fd26fef42ca
jeff buenting <jeffbuenting@gmail.com>
2022-11-19 14:30:23 -0500

    comment ssh and key

--- terraform up and running/ch2/main.tf
+++ terraform up and running/ch2/main.tf
@@ -2,8 +2,8 @@
 terraform {
     required_providers {
         aws = {
-            source = "hasicorp/aws"
-            version = "-> 4.0"
+            source = "hashicorp/aws"
+            version = "~> 4.0"
         }
     }
 }
@@ -12,19 +12,17 @@ provider "aws" {
     region = "us-east-1"
 }
 
-
 resource "aws_instance" "example" {
-    # ami = "ami-08c40ec9ead489470"
-    ami = "ami-0fb653ca2d3203ac1"
+    ami = "ami-08c40ec9ead489470"
     instance_type = "t2.micro"
-
+    # key_name = "EC2 SSH"
     vpc_security_group_ids = [aws_security_group.instance.id]
 
     user_data = <<-EOF
-        #!/bash/bin
-        echo "Hello, World" > index.html
-        nohub busybox httpd -f -p 8080 &
-        EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
     tags = {
         Name = "terraform-example"
@@ -40,4 +38,11 @@ resource "aws_security_group" "instance" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+
+    # ingress {
+    #     from_port = 22
+    #     to_port = 22
+    #     protocol = "tcp"
+    #     cidr_blocks = ["0.0.0.0/0"]
+    # }
 }
