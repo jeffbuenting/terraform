306848cc3a2e3b45b5f16ce78adfd447f0f578ea
jeff buenting <jeffbuenting@gmail.com>
2022-11-21 11:58:38 -0500

    forgot variable

--- terraform up and running/ch2/WebServerCluster/main.tf
+++ terraform up and running/ch2/WebServerCluster/main.tf
@@ -11,6 +11,11 @@ provider "aws" {
     region = "us-east-1"
 }
 
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    type = number
+}
+
 data "aws_vpc" "default" {
     default = true
 }
