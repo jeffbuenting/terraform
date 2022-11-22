72fc2caa4e5cdf0a98a3c66c38d06d42fc87bcb8
jeff buenting <jeffbuenting@gmail.com>
2022-11-22 16:07:37 -0500

    errors with depricating

---
+++ Backend/main.tf
@@ -0,0 +1,46 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 4.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform-state" {
+    bucket = "jdb-terraform-state"
+
+    # prevent accidental deleteion of this S3 bucket
+    lifecycle {
+        prevent_destroy = true
+    }
+
+    # Enable versioning so we can see the full revision history of our state files
+    versioning {
+        enabled = true
+    }
+
+    # Enable server-side encryption by default
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
+    }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+    name = "terraform-locks"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
