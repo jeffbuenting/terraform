b200de39718a9a23ec3acfacc6a5e239e0c56af0
jeff buenting <jeffbuenting@gmail.com>
2022-11-23 10:22:53 -0500

    backend now in S3

--- Backend/main.tf
+++ Backend/main.tf
@@ -1,33 +1,33 @@
 terraform {
-    backend "s3" {
-      bucket = "jdb-terraform-state"
-      key = "global/s3/terraform.tfstate"
-      region = "us-east-1"
+  backend "s3" {
+    bucket = "jdb-terraform-state"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-1"
 
-      dynamodb_table = "terraform-locks"
-      encrypt = true
-    }
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
 
-    
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 4.0"
-        }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "terraform-state" {
-    bucket = "jdb-terraform-state"
+  bucket = "jdb-terraform-state"
 
-    # prevent accidental deleteion of this S3 bucket
-    lifecycle {
-        prevent_destroy = true
-    }
+  # prevent accidental deleteion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_s3_bucket_acl" "terraform-state-acl" {
@@ -68,12 +68,14 @@ resource "aws_s3_bucket_public_access_block" "terraform-state-access-policy" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-    name = "terraform-locks"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "LockID"
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
-    attribute {
-        name = "LockID"
-        type = "S"
-    }
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
+
+
---
+++ terraform up and running/mysql/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  backend "s3" {
+    bucket = "jdb-terraform-state"
+    key    = "stage/services/mysql/terraform.tfstate"
+    region = "us-east-1"
+
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_instance" "example" {
+  identifier_prefix = "terraform-up-and-running"
+  engine            = "mysql"
+  allocated_storage = 10
+  instance_class    = "db.t2.micro"
+  name              = "example_database"
+  username          = "admin"
+
+  # How should we set the password?
+  password = "???"
+}
