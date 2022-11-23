9f6cf00d5d6d7cdffeeb5c37492a65bec62046b0
jeff buenting <jeffbuenting@gmail.com>
2022-11-22 20:50:57 -0500

    working S3 remote backend

--- Backend/main.tf
+++ Backend/main.tf
@@ -18,20 +18,34 @@ resource "aws_s3_bucket" "terraform-state" {
     lifecycle {
         prevent_destroy = true
     }
+}
 
-    # Enable versioning so we can see the full revision history of our state files
-    versioning {
-        enabled = true
-    }
+resource "aws_s3_bucket_acl" "terraform-state-acl" {
+  bucket = aws_s3_bucket.terraform-state.id
+  acl    = "private"
+}
 
-    # Enable server-side encryption by default
-    server_side_encryption_configuration {
-        rule {
-            apply_server_side_encryption_by_default {
-                sse_algorithm = "AES256"
-            }
-        }
+resource "aws_s3_bucket_versioning" "Terraform-state-version" {
+  bucket = aws_s3_bucket.terraform-state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform-state-encryption" {
+  bucket = aws_s3_bucket.terraform-state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "AES256"
     }
+  }
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
