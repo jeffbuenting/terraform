4ee1ebf08e6c555b89ad91a93bdfde08e3c074ea
jeff buenting <jeffbuenting@gmail.com>
2022-11-23 14:10:42 -0500

    moved userdata to exernal file

--- terraform up and running/WebServerCluster/main.tf
+++ terraform up and running/WebServerCluster/main.tf
@@ -30,18 +30,23 @@ data "terraform_remote_state" "db" {
   }
 }
 
+data "template_file" "user_data" {
+  template = file("user-data.sh")
+
+  vars = {
+    server_port = var.server_port
+    db_adress   = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
+  }
+
+}
+
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-08c40ec9ead489470"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instance.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              echo "$(data.terraform_remote_state.db.outputs.address)" >> index.html
-              echo "$(data.terraform_remote_state.db.outputs.port)" >> index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
+  user_data = data.template_file.user_data.rendered
 
   # Required when using a launch configuration with an auto scaling group.
   lifecycle {
