5a6596cbf6190ca6e7441bf64bbe979db11a69a8
jeff buenting <jeffbuenting@gmail.com>
2022-11-23 14:21:07 -0500

    address typo

--- terraform up and running/WebServerCluster/main.tf
+++ terraform up and running/WebServerCluster/main.tf
@@ -35,7 +35,7 @@ data "template_file" "user_data" {
 
   vars = {
     server_port = var.server_port
-    db_adress   = data.terraform_remote_state.db.outputs.address
+    db_address  = data.terraform_remote_state.db.outputs.address
     db_port     = data.terraform_remote_state.db.outputs.port
   }
 
