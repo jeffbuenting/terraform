44df6fac4762f7c4235b44020d78bf14653ba50d
jeff buenting <jeffbuenting@gmail.com>
2022-11-23 08:54:05 -0500

    configured backend state

--- Backend/main.tf
+++ Backend/main.tf
@@ -1,4 +1,14 @@
 terraform {
+    backend "s3" {
+      bucket = "jdb-terraform-state"
+      key = "global/s3/terraform.tfstate"
+      region = "us-east-1"
+
+      dynamodb_table = "terraform-locks"
+      encrypt = true
+    }
+
+    
     required_providers {
         aws = {
             source = "hashicorp/aws"
