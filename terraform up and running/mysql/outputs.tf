4533a2bde14b3ab457a9cb42efa485b9dfc4e211
jeff buenting <jeffbuenting@gmail.com>
2022-12-01 14:42:43 -0500

    reset to start over

--- terraform up and running/mysql/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "address" {
-  value       = aws_db_instance.example.address
-  description = "Connect to the database at the endpoint"
-}
-
-output "port" {
-  value       = aws_db_instance.example.port
-  description = "the port the database is listening on"
-}
