9924b65c510d6f81a982715ceb147cca3e21c5ba
jeff buenting <jeffbuenting@gmail.com>
2022-12-01 09:06:45 -0500

    back to single module

--- terraform up and running/modules/services/webserver-cluster/outputs.tf
+++ terraform up and running/modules/services/webserver-cluster/outputs.tf
@@ -8,8 +8,12 @@
 #   description = "The name of the auto scaling group"
 # }
 
-output "alb_dns_name" {
-  value       = aws_lb.example.dns_name
-  description = "The domain name of the load balancer"
-}
+# output "alb_dns_name" {
+#   value       = aws_lb.example.dns_name
+#   description = "The domain name of the load balancer"
+# }
 
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of he web server."
+}
